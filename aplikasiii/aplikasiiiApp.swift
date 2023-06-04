931c1c6f5ccce93930a7fec3ffe6cfc602ff6dd2
Billbert1705 <billbertpohandy@icloud.com>
2023-06-04 15:07:27 +0700

    Initial Commit

---
+++ aplikasiii/aplikasiiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  aplikasiiiApp.swift
+//  aplikasiii
+//
+//  Created by Billbert Pohandy on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct aplikasiiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
