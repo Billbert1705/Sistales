d0d5f889ddad419ecada3797a4f3622b31466dca
Billbert1705 <billbertpohandy@icloud.com>
2023-06-05 18:01:13 +0700

    code sampai 7.1 billbert

--- aplikasiii/ContentView.swift
+++ aplikasiii/ContentView.swift
@@ -201,7 +201,7 @@ struct Screen3: View {
 
 struct Screen4: View {
     @Binding var isNextScreenActive: Bool
-    @State private var isScreen4Active = false
+    @State private var isScreen5Active = false
     var body: some View {
         VStack {
             HStack {
@@ -218,24 +218,227 @@ struct Screen4: View {
         .background(Image("scene4")
             .resizable()
             .frame(width: 390, height: 856.5))
+        .onTapGesture {
+            isScreen5Active = true
+        }
+        .background(
+            NavigationLink(
+                destination: Screen5(isNextScreenActive: $isNextScreenActive),
+                isActive: $isScreen5Active
+            ) {
+                EmptyView()
+            }
+            .hidden()
+        )
+        .navigationBarHidden(true)
+    }
+    }
+
+struct Screen5: View {
+    @Binding var isNextScreenActive: Bool
+    @State private var isScreen6Active = false
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image("imageSound")
+                    .resizable()
+                    .frame(width: 43, height: 43)
+            }
+            Spacer()
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("scene5")
+            .resizable()
+            .frame(width: 390, height: 856.5))
+        .onTapGesture {
+            isScreen6Active = true
+        }
+        .background(
+            NavigationLink(
+                destination: Screen6(isNextScreenActive: $isNextScreenActive),
+                isActive: $isScreen6Active
+            ) {
+                EmptyView()
+            }
+                .hidden()
+        )
+        .navigationBarHidden(true)
+    }
+}
+struct Screen6: View {
+    @Binding var isNextScreenActive: Bool
+    @State private var isScreen7Active = false
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image("imageSound")
+                    .resizable()
+                    .frame(width: 43, height: 43)
+            }
+            Spacer()
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("scene6")
+            .resizable()
+            .frame(width: 390, height: 856.5))
+        .onTapGesture {
+            isScreen7Active = true
+        }
+        .background(
+            NavigationLink(
+                destination: Screen7(isNextScreenActive: $isNextScreenActive),
+                isActive: $isScreen7Active
+            ) {
+                EmptyView()
+            }
+                .hidden()
+        )
+        .navigationBarHidden(true)
+    }
+}
+struct Screen7: View {
+    @Binding var isNextScreenActive: Bool
+    @State private var isScreen8Active = false
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image("imageSound")
+                    .resizable()
+                    .frame(width: 43, height: 43)
+            }
+            Spacer()
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("scene7")
+            .resizable()
+            .frame(width: 390, height: 856.5))
+        .onTapGesture {
+            isScreen8Active = true
+        }
+        .background(
+            NavigationLink(
+                destination: Screen8(isNextScreenActive: $isNextScreenActive),
+                isActive: $isScreen8Active
+            ) {
+                EmptyView()
+            }
+                .hidden()
+        )
+        .navigationBarHidden(true)
+    }
+}
+struct Screen8: View {
+    @Binding var isNextScreenActive: Bool
+    @State private var isPopUpVisible = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image("imageSound")
+                    .resizable()
+                    .frame(width: 43, height: 43)
+            }
+            Spacer()
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            Image("scene71")
+                .resizable()
+                .frame(width: 390, height: 856.5)
+                .onTapGesture {
+                    isPopUpVisible = true
+                }
+                .overlay(
+                    VStack {
+                        if isPopUpVisible {
+                            ZStack {
+                                Color.black.opacity(0.6)
+                                    .edgesIgnoringSafeArea(.all)
+                                VStack(spacing: 20) {
+                                    Button(action: {
+                                        isNextScreenActive = false // Set to false first to close the pop-up
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                            isNextScreenActive = true // Set to true to navigate to Screen 9
+                                        }
+                                    }) {
+                                        Text("Go to Screen 9")
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color.blue)
+                                            .cornerRadius(10)
+                                    }
+                                    Button(action: {
+                                        // Handle action for Screen 10
+                                    }) {
+                                        Text("Go to Screen 10")
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color.green)
+                                            .cornerRadius(10)
+                                    }
+                                }
+                            }
+                            .transition(.opacity)
+                        }
+                    }
+                )
+        )
+        .navigationBarHidden(true)
+    }
+}
+
+struct Screen9: View {
+    @Binding var isNextScreenActive: Bool
+    @State private var isScreen10Active = false
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image("imageSound")
+                    .resizable()
+                    .frame(width: 43, height: 43)
+            }
+            Spacer()
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("scene9")
+            .resizable()
+            .frame(width: 390, height: 856.5))
 //        .onTapGesture {
-//            isScreen4Active = true
+//            isScreen10Active = true
 //        }
 //        .background(
 //            NavigationLink(
-//                destination: Screen5(isNextScreenActive: $isNextScreenActive),
-//                isActive: $isScreen5Active
+//                destination: Screen8(isNextScreenActive: $isNextScreenActive),
+//                isActive: $isScreen10Active
 //            ) {
 //                EmptyView()
 //            }
-//            .hidden()
+//                .hidden()
 //        )
 //        .navigationBarHidden(true)
-//    }
+        }
     }
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
         }
     }
-}
+
+
+
+    
