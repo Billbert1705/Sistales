0044cf96cdb8f162e455a96c455318b438ce3147
qonitcah <qonitcah@gmail.com>
2023-06-08 23:44:27 +0700

    push terus

--- aplikasiii/screenqoni2.swift
+++ aplikasiii/screenqoni2.swift
@@ -11,50 +11,57 @@ struct screenqoni2: View {
     @State private var size = UIScreen.main.bounds.size
     var body: some View {
         NavigationStack{
-            ZStack{
-                Image("diniKram")
-                    .resizable()
-                    .ignoresSafeArea()
-                VStack {
-                    HStack{
-                        NavigationLink{
-                            edukasi_2()
-                        }label: {
-                            Image(systemName: "chevron.left")
+            NavigationLink{
+                screenqoni3()
+            }label: {
+                ZStack{
+                    Image("diniKram")
+                        .resizable()
+                        .ignoresSafeArea()
+                    VStack {
+                        HStack{
+                            NavigationLink{
+                                edukasi_2()
+                            }label: {
+                                Image(systemName: "chevron.left")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 34)
+                                    .padding(.horizontal, 20)
+                                    .foregroundColor(.black)
+                            }
+                            Image("imageSound")
                                 .resizable()
-                                .scaledToFit()
-                                .frame(height: 34)
-                                .padding(.horizontal, 20)
-                                .foregroundColor(.black)
+                                .frame(width: 43, height: 43)
+                                .padding(.horizontal, 15)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
                         }
-                        Image("imageSound")
-                            .resizable()
-                            .frame(width: 43, height: 43)
-                            .padding(.horizontal, 15)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
+                        Spacer()
                     }
-                    Spacer()
-                }
-                    
-                VStack{
-                    Spacer()
-                    ZStack{
-                        Image("bubblekiri")
-                            .resizable()
-                            .frame(width: 343, height: 87)
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(width: 343, height: 80)
-                            .foregroundColor(.white)
-                        Text("Aduh, perut aku kram banget karena menstruasi. Sakit banget!")
-                            .font(.system(size: 17))
-                            .padding(.top,5)
-                            .padding(.horizontal, 25)
+                        
+                    VStack{
+                        Spacer()
+                        ZStack{
+                            Image("bubblekiri")
+                                .resizable()
+                                .frame(width: 343, height: 87)
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 343, height: 80)
+                                .foregroundColor(.white)
+                            Text("Aduh, perut aku kram banget karena menstruasi. Sakit banget!")
+                                .font(.system(size: 17))
+                                .padding(.top,5)
+                                .padding(.horizontal, 25)
+                        }
+                        .frame(width: 343, height: 141)
+                        .padding(.bottom, 40)
+                        
                     }
-                    .frame(width: 343, height: 141)
-                    .padding(.bottom, 40)
-                    
                 }
+                .foregroundColor(.black)
             }
+            .buttonStyle(PlainButtonStyle())
+            
         }
         
     }
